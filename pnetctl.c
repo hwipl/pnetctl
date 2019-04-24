22a2f107106e2505dc4523c88450a9fe69201c2d
hwipl <33433250+hwipl@users.noreply.github.com>
2019-04-24 22:58:33 +0200

    output pnetids of devices and reformat program output
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- pnetctl.c
+++ pnetctl.c
@@ -169,6 +169,9 @@ struct device {
 
 	/* pnetid */
 	char pnetid[SMC_MAX_PNETID_LEN + 1];
+
+	/* terminal output */
+	int output;
 };
 
 /* list of devices */
@@ -442,9 +445,104 @@ int udev_scan_devices() {
  * *****************
  */
 
+/* print a horizontal line on screen */
+void print_line() {
+	printf("----------------------------------------------------------");
+	printf("----\n");
+}
+
+/* print a horizontal bold line on screen */
+void print_bold_line() {
+	printf("==========================================================");
+	printf("====\n");
+}
+
+/* print the header on screen */
+void print_header() {
+	print_bold_line();
+	printf("%-16s %5.5s %15.15s %6.6s %16.16s\n", "Pnetid:", "Type:",
+	       "Name:", "Port:", "PCI-ID:");
+	print_bold_line();
+}
+
+/* print the pnetid on screen */
+void print_pnetid(char *pnetid) {
+	printf("%s\n", pnetid);
+	print_line();
+}
+
+/* print device on screen */
+void print_device(struct device *device) {
+	printf("%-16s", "");
+	if (!strncmp(device->subsystem, "infiniband", 10)) {
+		printf(" %5.5s", "ib");
+		printf(" %15.15s", device->name);
+		printf(" %6d", device->ib_port);
+	} else {
+		printf(" %5.5s", device->subsystem);
+		printf(" %15.15s", device->name);
+		printf(" %6.6s", "");
+	}
+	if (device->parent)
+		printf(" %16.16s", device->parent);
+	else
+		printf(" %16.16s", "n/a");
+	printf("\n");
+}
+
+/* print all devices on screen */
+void print_device_table() {
+	int pnetid_found = 1;
+	struct device *next;
+	char *pnetid;
+
+	/* print header first */
+	print_header();
+
+	/* print each pnetid and devices with same pnetid */
+	while (pnetid_found) {
+		pnetid_found = 0;
+		pnetid = NULL;
+		next = get_next_device(&devices_list);
+		while (next) {
+			/* skip devices already output and without pnetid */
+			if (next->output || !next->pnetid[0]) {
+				next = get_next_device(next);
+				continue;
+			}
+			if (pnetid_found) {
+				/* another device with same pnetid? */
+				if (!strncmp(next->pnetid, pnetid,
+					     SMC_MAX_PNETID_LEN)) {
+					print_device(next);
+					next->output = 1;
+				}
+			} else {
+				/* found new pnetid in list */
+				pnetid_found = 1;
+				pnetid = next->pnetid;
+				print_pnetid(pnetid);
+				print_device(next);
+				next->output = 1;
+			}
+			next = get_next_device(next);
+		}
+		if (pnetid_found)
+			print_line();
+	}
+
+	/* print remaining devices */
+	print_pnetid("n/a");
+	next = get_next_device(&devices_list);
+	while (next) {
+		if (!next->output)
+			print_device(next);
+		next = get_next_device(next);
+	}
+}
+
 /* main function */
 int main() {
-	struct device *next;
 	int rc;
 
 	/* get all devices via udev and put them in devices list */
@@ -459,30 +557,8 @@ int main() {
 	nl_get_pnetids();
 	nl_cleanup();
 
-	/* handle each device in devices list */
-	printf("%18.18s %10.10s %15.15s %16.16s %15.15s %9.9s\n", "Pnetid:",
-	       "Type:", "Name:", "PCI-ID:", "Lowest:", "IB-Port:");
-	next = get_next_device(&devices_list);
-	while (next) {
-		printf("%18.18s", next->pnetid);
-		printf(" %10.10s", next->subsystem);
-		printf(" %15.15s", next->name);
-		if (next->parent)
-			printf(" %16.16s", next->parent);
-		else
-			printf(" %16.16s", "n/a");
-		if (next->lowest)
-			printf(" %15.15s", next->lowest);
-		else
-			printf(" %15.15s", "n/a");
-		if (next->ib_port != -1)
-			printf(" %9d", next->ib_port);
-		else
-			printf(" %9.9s", "n/a");
-		printf("\n");
-
-		next = get_next_device(next);
-	}
+	/* print devices to terminal */
+	print_device_table();
 
 	/* free devices in devices list */
 	free_devices();
