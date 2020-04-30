ed24ee3e121b282a359dfa98c7d2c4f0a422e0d7
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-30 19:41:22 +0200

    add more print tests
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/print_test.c
+++ src/print_test.c
@@ -9,6 +9,7 @@
 #include "print.h"
 #include "common.h"
 #include "udev.h"
+#include "netlink.h"
 
 // test the function print_device_table()
 int test_print_device_table() {
@@ -30,6 +31,23 @@ int test_print_device_table() {
 	// filled device table, (not matching) filter
 	pnetid_filter = "DOES_NOT_MATCH";
 	print_device_table();
+
+	// add a pnetid and get pnetids
+	nl_init();
+	nl_set_pnetid("PNETCTL", "lo", NULL, 0);
+	nl_get_pnetids();
+
+	// filled device table, no filter
+	pnetid_filter = NULL;
+	print_device_table();
+
+	// filled device table, filter
+	pnetid_filter = "PNETCTL";
+	print_device_table();
+
+	// cleanup
+	nl_del_pnetid("PNETCTL");
+	nl_cleanup();
 
 	return 0;
 }
