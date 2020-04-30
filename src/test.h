45d07c43d2176a5d6c8a96bee3917b33b7575ab0
hwipl <33433250+hwipl@users.noreply.github.com>
2020-04-30 13:15:11 +0200

    add src/test.h
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ src/test.h
@@ -0,0 +1,41 @@
+#ifndef _PNETCTL_TEST_H
+#define _PNETCTL_TEST_H
+
+#include <string.h>
+#include <stdio.h>
+
+/* test structure: name of test and the test's function */
+struct test {
+	const char *name;
+	int (* func)();
+};
+
+
+/* run a specific test with name in tests or all tests if name is NULL */
+int run_test(struct test tests[], const char *name) {
+	int rc;
+
+	// no command line argument -> run all tests
+	if (!name) {
+		for (int i=0; tests[i].name != NULL; i++) {
+			printf("Testing %s.\n", tests[i].name);
+			rc = tests[i].func();
+			if (rc) {
+				return rc;
+			}
+		}
+		return 0;
+	}
+
+	// run specific test given as first command line argument
+	for (int i=0; tests[i].name != NULL; i++) {
+		if (!strcmp(tests[i].name, name)) {
+			printf("Testing %s.\n", tests[i].name);
+			return tests[i].func();
+		}
+	}
+	printf("Test not found.\n");
+	return -1;
+}
+
+#endif
